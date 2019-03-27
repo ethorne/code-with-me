2c645f7e12d1e6b5d20007bc7ec222f170377a37
Erik Thorne <erik.j.thorne@gmail.com>
2019-03-26 19:17:16 -0700

    start work on client side bundler

---
+++ client/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: 'src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'env',
+              'react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename: 'main.css'})
+  ]
+}
