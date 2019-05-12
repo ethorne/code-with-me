a09b873b50536000595f76714b22a980f3ecf18c
Brandon Paris <brandon@Brandons-MBP.voxel.private>
2019-05-12 18:18:33 -0400

    Add webpack dev config, prod config, new build/start scripts, move server around

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,9 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './css/index.css';
-import Rover from './codeWithMe.js'
+import CodeWithMe from './codeWithMe.js'
 
+console.log('rendering from index.js');
 ReactDOM.render(
   <CodeWithMe />,
   document.getElementById('root')
