c1bd3eaa719e2b7d82bd8f11b468246770e85032
Erik Thorne <erik.j.thorne@gmail.com>
2019-05-12 09:06:18 -0700

    why doesn't it load?!

---
+++ client/src/codeWithMe.js
@@ -0,0 +1,19 @@
+class CodeWithMe extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state =  { data: { } }
+  }
+
+  render() {
+    console.log('\n\n\n\t\tLOADED\n\n\n');
+    return (
+      <div className="code-with-me">
+        <h1>hello world!</h1>
+      </div>
+    );
+  }
+
+}
+
+export default CodeWithMe;
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './css/index.css';
+import Rover from './codeWithMe.js'
+
+ReactDOM.render(
+  <CodeWithMe />,
+  document.getElementById('root')
+);
